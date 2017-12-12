793e7aba7933496833314be44abd39f32dfe5454
GIOVANNY <GIOVANNY@172.16.22.115>
2017-12-11 20:35:27 -0500

    Ejemplo de Java Properties

---
+++ Unidad4-prueba2/src/main/java/com/lds/Servlet1.java
@@ -0,0 +1,47 @@
+package com.lds;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doPost(request, response);
+		response.getWriter().print(System.getProperties().toString().replaceAll("," , "<br>"));
+//		String dato =new String("");
+//		dato = dato.replace("SE" , "GIO");
+//		System.out.println(dato.toString());
+		
+	}
+	//String quitarComas(String cadena) {
+		//return cadena.replaceAll(",", "\n"); 
+	//}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
