560dd81e8b978836ff60f795922a2a8ebd885ccd
GIOVANNY <GIOVANNY@172.16.22.115>
2017-12-13 20:58:25 -0500

    Prueba 4

---
+++ Unidad4-prueba4/src/main/java/com/lds/soap/App.java
@@ -0,0 +1,34 @@
+package com.lds.soap;
+
+import java.rmi.RemoteException;
+
+import com.lds.soap.GlobalWeatherStub;
+import com.lds.soap.GlobalWeatherStub.GetCitiesByCountry;
+import com.lds.soap.GlobalWeatherStub.GetCitiesByCountryResponse;
+import com.lds.soap.GlobalWeatherStub.GetWeather;
+import com.lds.soap.GlobalWeatherStub.GetWeatherResponse;
+
+public class App {
+
+	public static void main(String[] args) throws RemoteException {
+       
+		GlobalWeatherStub globalWeatherStub = new GlobalWeatherStub();
+        
+		GetCitiesByCountry getCitiesByCountry = new GetCitiesByCountry();
+		getCitiesByCountry.setCountryName("Colombia");
+		
+		GetCitiesByCountryResponse getCitiesByCountryResponse = globalWeatherStub
+				.getCitiesByCountry(getCitiesByCountry);
+		
+		System.out.println(getCitiesByCountryResponse.getGetCitiesByCountryResult());
+		
+		GetWeather getWeather = new GetWeather();
+		getWeather.setCityName("Medellin");
+		getWeather.setCityName("Colombia");
+		GetWeatherResponse getWeatherResponse = globalWeatherStub.getWeather(getWeather);
+		
+		System.out.println(getWeatherResponse.getGetWeatherResult());
+
+	}
+
+}
